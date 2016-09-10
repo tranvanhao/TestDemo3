02faee0e10a2894e55c1dc59c55b7686ca527f23
Nguyễn Mạnh Tiến <nmtien92@outlook.com>
2016-09-10 13:41:25 +0700

    Initial Commit

---
+++ Ex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ex
+//
+//  Created by nmtien92 on 9/10/16.
+//  Copyright © 2016 nmtien92. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExTests/ExTests.m
@@ -0,0 +1,39 @@
+//
+//  ExTests.m
+//  ExTests
+//
+//  Created by nmtien92 on 9/10/16.
+//  Copyright © 2016 nmtien92. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ExTests : XCTestCase
+
+@end
+
+@implementation ExTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
